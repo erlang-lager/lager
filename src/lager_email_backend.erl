9bbee2b7e442ba75fc4a3bd6fa09f423bbb1779f
Satyam Shekhar <satyamshekhar@gmail.com>
2012-08-26 20:26:49 +0530

    Enabling throttle - emails per min.

--- src/lager_email_backend.erl
+++ src/lager_email_backend.erl
@@ -16,17 +16,16 @@
 -record(state,{
           from :: string(),
           to :: string(),
-          subject :: string(),
           username :: string(),
           password :: string(),
           level :: integer(),
           host  :: string(),
-          port  :: integer(),
-          counter :: integer()
+          count :: integer(),
+          emails_per_min :: integer(),
+          last_sent_at
          }).
 
-% Config is a prop list
-init([LogLevel, From, To, Username, Passwd, Host]) ->
+init([LogLevel, From, To, Username, Passwd, Host, EmailsPerMin]) ->
     case lists:member(LogLevel, ?LEVELS) of
         true ->
             {ok, #state{
@@ -36,7 +35,9 @@ init([LogLevel, From, To, Username, Passwd, Host]) ->
                     username = Username,
                     password = Passwd,
                     host = Host,
-                    counter = 0
+                    emails_per_min = EmailsPerMin,
+                    count = 0,
+                    last_sent_at = 0
                    }};
         _ ->
             {error, bad_log_level}
@@ -56,31 +57,35 @@ handle_call(_Request, State) ->
     {ok, ok, State}.
 
 %% @private
-handle_event({log, Level, {Date, Time}, [LevelStr, Location, Message]},
-    #state{level=LogLevel, from = From, to = To, host = Host,
-      username = Username, password = Password} = State) 
-  when Level =< LogLevel ->
-
-    Hostname = net_adm:localhost(),
-    Subject = [atom_to_list(?NUM2LEVEL(LogLevel)), " on ", Hostname],
-    Headers = [
-        header("from: ", From),
-        header("to: ", format_to(To)),
-        header("subject: ", Subject),
-        header("Content-Type: ", "text/plain; charset=utf-8"),
-        header("Content-Transfer-Encoding: ", "7bit"),
-        header("Content-Disposition: ", "inline"),
-        "\r\n"
-    ],
-    EmailBody = [Date, " ", Time, " ", LevelStr, Location, Message],
-    gen_smtp_client:send({From, To, [Headers, EmailBody]},
-        [{relay, Host},
-         {username, Username},
-         {password, Password},
-         {no_mx_lookups, true}]),
+handle_event({log, Level, DateTime, Log},
+  #state{level = LogLevel, emails_per_min = EmailsPerMin} = State)
+  when EmailsPerMin =:= 0, Level =< LogLevel -> 
+    send_email(DateTime, Log, State),
     {ok, State};
+
+handle_event({log, Level, DateTime, Log},
+  #state{level = LogLevel, last_sent_at = LastSentAt,
+         count = Count, emails_per_min = EmailsPerMin} = State)
+  when Level =< LogLevel ->
+    Now = timestamp(),
+    State2 = case Now - LastSentAt > 60 of
+        true ->
+            send_email(DateTime, Log, State),
+            State#state{last_sent_at = Now, count = 1};
+        false ->
+            case Count < EmailsPerMin of
+                true ->
+                    send_email(DateTime, Log, State),
+                    State#state{count = Count + 1};
+                false ->
+                    State
+            end
+        end,
+    {ok, State2};
+
 handle_event(_Event, State) ->
     {ok, State}.
+
 
 %% @private
 handle_info(_Info, State) ->
@@ -109,3 +114,30 @@ format_to2([H|T], Acc) ->
     format_to2(T, [[H, ","] | Acc]);
 format_to2([], Acc) ->
     Acc.
+
+%% @private
+timestamp() ->
+    calendar:datetime_to_gregorian_seconds(
+      calendar:now_to_universal_time(now())).
+
+%% @private
+send_email({Date, Time}, [LevelStr, Location, Message],
+    #state{from = From, to = To, host = Host, username = Username, 
+        password = Password}) ->
+    Hostname = net_adm:localhost(),
+    Subject = [LevelStr, " on ", Hostname],
+    Headers = [
+        header("from: ", From),
+        header("to: ", format_to(To)),
+        header("subject: ", Subject),
+        header("Content-Type: ", "text/plain; charset=utf-8"),
+        header("Content-Transfer-Encoding: ", "7bit"),
+        header("Content-Disposition: ", "inline"),
+        "\r\n"
+    ],
+    EmailBody = [Date, " ", Time, " ", LevelStr, Location, Message],
+    gen_smtp_client:send({From, To, [Headers, EmailBody]},
+        [{relay, Host},
+         {username, Username},
+         {password, Password},
+         {no_mx_lookups, true}]).
