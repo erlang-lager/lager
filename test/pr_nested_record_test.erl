773e4439be0314adf2acf559b426fb7e9a7b85e7
Maxim Minin <maxim@mir-desktop.(none)>
2013-09-02 19:05:30 +0200

    simple test for pretty print of nested records.

---
+++ test/pr_nested_record_test.erl
@@ -0,0 +1,21 @@
+-module(pr_nested_record_test).
+
+-compile([{parse_transform, lager_transform}]).
+
+-record(a, {field1, field2}).
+-record(b, {field1, field2}).
+
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+nested_record_test() ->
+    A = #a{field1 = x, field2 = y}, 
+    B = #b{field1 = A, field2 = z},
+    Pr_B = lager:pr(B, ?MODULE),
+    ?assertEqual({'$lager_record', b,
+                    [{field1, {'$lager_record', a,
+                                    [{field1, x},{field2, y}]}},
+                     {field2, z}]}, 
+                   Pr_B).
