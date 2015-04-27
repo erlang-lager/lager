f37a957a0e2fda9cc2fbd8ee206a4132aee3df70
John R. Daily <git@epep.us>
2015-04-27 10:21:49 -0400

    Support extra sink argument

--- test/lager_crash_backend.erl
+++ test/lager_crash_backend.erl
@@ -27,7 +27,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
-init([CrashBefore, CrashAfter]) ->
+init([{sink, _Sink}, CrashBefore, CrashAfter]) ->
     case is_tuple(CrashBefore) andalso (timer:now_diff(CrashBefore, os:timestamp()) > 0) of
         true ->
             %?debugFmt("crashing!~n", []),
