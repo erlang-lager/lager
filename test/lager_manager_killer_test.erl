2d0351f0485b35cc327da8fb9dc791de6a3b1700
Brian L. Troutwine <brian.troutwine@adroll.com>
2016-03-17 13:06:02 -0700

    Correct test failures for lager_handler_watcher.
    Signed-off-by: Brian L. Troutwine <brian.troutwine@adroll.com>

--- test/lager_manager_killer_test.erl
+++ test/lager_manager_killer_test.erl
@@ -6,38 +6,42 @@
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
-overload_test() ->
-    application:stop(lager),
-    application:load(lager),
-    Delay = 1000, % sleep 1 sec on every log
-    KillerHWM = 10, % kill the manager if there are more than 10 pending logs
-    KillerReinstallAfter = 1000, % reinstall killer after 1 sec
-    application:set_env(lager, handlers, [{lager_slow_backend, [{delay, Delay}]}]),
-    application:set_env(lager, async_threshold, undefined),
-    application:set_env(lager, killer_hwm, KillerHWM),
-    application:set_env(lager, killer_reinstall_after, KillerReinstallAfter),
-    ensure_started(lager),
-    lager_config:set(async, true),
-    Manager = whereis(lager_event),
-    erlang:trace(all, true, [procs]),
-    [lager:info("~p'th message", [N]) || N <- lists:seq(1,KillerHWM+2)],
-    Margin = 100,
-    ok = confirm_manager_exit(Manager, Delay+Margin),
-    ok = confirm_sink_reregister(Margin),
-    erlang:trace(all, false, [procs]),
-    wait_until(fun() ->
-                       case proplists:get_value(lager_manager_killer, gen_event:which_handlers(lager_event)) of
-                           [] -> false;
-                           _ -> true
-                       end
-               end, Margin, 15),
-    wait_until(fun() ->
-                       case gen_event:call(lager_event, lager_manager_killer, get_settings) of
-                           [KillerHWM, KillerReinstallAfter] -> true;
-                           _Other -> false
-                       end
-               end, Margin, 15),
-    application:stop(lager).
+overload_test_() ->
+    {timeout, 60,
+     fun() ->
+             application:stop(lager),
+             application:load(lager),
+             Delay = 1000, % sleep 1 sec on every log
+             KillerHWM = 10, % kill the manager if there are more than 10 pending logs
+             KillerReinstallAfter = 1000, % reinstall killer after 1 sec
+             application:set_env(lager, handlers, [{lager_slow_backend, [{delay, Delay}]}]),
+             application:set_env(lager, async_threshold, undefined),
+             application:set_env(lager, error_logger_redirect, true),
+             application:set_env(lager, killer_hwm, KillerHWM),
+             application:set_env(lager, killer_reinstall_after, KillerReinstallAfter),
+             ensure_started(lager),
+             lager_config:set(async, true),
+             Manager = whereis(lager_event),
+             erlang:trace(all, true, [procs]),
+             [lager:info("~p'th message", [N]) || N <- lists:seq(1,KillerHWM+2)],
+             Margin = 100,
+             ok = confirm_manager_exit(Manager, Delay+Margin),
+             ok = confirm_sink_reregister(Margin),
+             erlang:trace(all, false, [procs]),
+             wait_until(fun() ->
+                                case proplists:get_value(lager_manager_killer, gen_event:which_handlers(lager_event)) of
+                                    [] -> false;
+                                    _ -> true
+                                end
+                        end, Margin, 15),
+             wait_until(fun() ->
+                                case gen_event:call(lager_event, lager_manager_killer, get_settings) of
+                                    [KillerHWM, KillerReinstallAfter] -> true;
+                                    _Other -> false
+                                end
+                        end, Margin, 15),
+             application:stop(lager)
+     end}.
 
 ensure_started(App) ->
     case application:start(App) of
