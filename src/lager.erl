21caa253f3cfcbf1cf1e69494fddafc117d088b1
Andrew Thompson <andrew@hijacked.us>
2025-08-26 07:53:37 -0700

    Fix binding warning

--- src/lager.erl
+++ src/lager.erl
@@ -77,7 +77,7 @@ start(App) ->
     start_ok(App, application:start(App, permanent)).
 
 start_ok(_App, ok) -> ok;
-start_ok(_App, {error, {already_started, _App}}) -> ok;
+start_ok(_App, {error, {already_started, _App2}}) -> ok;
 start_ok(App, {error, {not_started, Dep}}) ->
     ok = start(Dep),
     start(App);
